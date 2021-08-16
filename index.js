f8e66f8fb9a1d37e3fc1dae5c79baf1c2d9debe6
EduJCordeiro <edujcordeiro99@gmail.com>
2021-08-16 19:52:52 -0300

    att

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+
+const json2html = require('node-json2html');
+ 
+
 var api = require('./api');
 
 var express = require('express');
@@ -6,7 +12,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8000;
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -20,6 +26,60 @@ app.use(bodyParser.json());
 
 // get api
 app.get('/api', api);
+
+app.get('/fiis', function (req, res) {
+  const url = 'https://www.fundsexplorer.com.br/ranking';
+
+  async function main() {
+    const result = await request.get(url);
+    const $ = cheerio.load(result);
+    const scrapedData = [];
+    const tableHeaders = [];
+
+    const teste = $("#table-ranking tr");
+    teste.each((index, element_main) => {
+      if (index === 0) {
+        const ths = $(element_main).find("th");
+        $(ths).each((i, element) => {
+          let names = $(element).text();
+          tableHeaders.push(
+            names.replace(/[&\\óô]/g, 'o')
+            .replace(/[&\\ç]/g, 'c')
+            .replace(/[&\\áãâ]/g, 'a')
+            .replace(/[&\\é]/g, 'e')
+            .replace(/[&\/\\().]/g, '')
+            .replace(/[&\\í]/g, 'i')
+            .replace(' ','')
+          );
+        });
+      }else{
+        const tds = $(element_main).find("td");
+        const tableRow = {};
+        $(tds).each((i, element) => {
+          tableRow[tableHeaders[i]] = $(element).text();
+        });
+        scrapedData.push(tableRow);
+        console.log(tableRow)
+      }
+    });
+    const row = html => `<tr>\n${html}</tr>\n`,
+      heading = object => row(Object.keys(object).reduce((html, heading) => (html + `<th>${heading}</th>`), '')),
+      datarow = object => row(Object.values(object).reduce((html, value) => (html + `<td>${value}</td>`), ''));
+                               
+    function htmlTable(dataList) {
+      return `<table>
+                ${heading(dataList[0])}
+                ${dataList.reduce((html, object) => (html + datarow(object)), '')}
+              </table>`
+    }
+        
+    let html = htmlTable(scrapedData);
+
+    res.send(html);
+   }
+
+   main();
+});
 
 // error handler
 app.use(function(err, req, res, next) {
